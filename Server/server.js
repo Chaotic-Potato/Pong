5bfb2a800d2173cc640c4499cde61795187db012
Sam Quinn <lazersmoke@gmail.com>
2016-05-06 17:57:22 -0500

    Added server behavior

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,6 @@
 "use strict";
 var Server = {
-	clients: [],
+  clients: [], 
 	tickRate: 100,
 	init: function() {
 		s.loop = setInterval(s.tick, (1000 / s.tickRate))
@@ -17,7 +17,26 @@ var Server = {
 			con.on('message', function(message) {
 				var m = JSON.parse(message.utf8Data)
 				typeFunc = {
-					
+          connect: function(data, con){
+            if(s.nameValid(data)){
+              con.name = data
+              s.send(con, "connected", con.name)
+              s.updateLobby()
+            }
+            else{
+              s.send(con, "fatalerror", "Your Username was invalid!")
+            }
+          },
+          pass: function(data, con){
+            for(i in s.clients){
+              if(s.clients[i].name == con.pair){
+                s.send(s.clients[i], "pairmessage", data)
+              }
+            }
+          },
+          pair: function(data, con){
+            con.pair = data
+          }
 				}
 				if (typeFunc[m.type]) {
 					typeFunc[m.type](m.data, con)
@@ -26,19 +45,24 @@ var Server = {
 			con.on('close', function(r, desc) {
 				for (i in s.clients) {
 					if (s.clients[i] == con) {
-						s.send("disconnect", s.clients[i].name)
 						s.clients.splice(i, 1)
+            s.updateLobby()
 					}
 				}
-				
 			})
 		})
 	},
-	send: function(t, m) {
+	sendall: function(t, m) {
 		for (x in s.clients){
 			s.clients[x].sendUTF(JSON.stringify({type : t, data : m}))
 		}
+  },
+	send: function(c, t, m) {
+  	c.sendUTF(JSON.stringify({type : t, data : m}))
 	},
+  updateLobby: function(){
+    s.sendall("lobby", s.clients)
+  },
 	tick: function() {}
 }
 
