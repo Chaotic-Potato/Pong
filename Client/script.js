c6a29271c065d3b04ff2e6dcaddb73c734092f38
Chaotic-Potato <andy825825@gmail.com>
2016-05-06 17:40:59 -0500

    Connect

--- Client/script.js
+++ Client/script.js
@@ -1,22 +1,22 @@
 var Client = {
 	tickRate: 100,
+	y: 0,
 	connect: function() {
 		c.loop = setInterval(c.tick, (1000 / c.tickRate))
 		if (c.sock == undefined){
 			c.sock = new WebSocket("ws://potatobox.no-ip.info:7664", 'echo-protocol')
-			c.name = get("name").value
+			c.name = get("name")
 			c.sock.onmessage = function (evt) { 
 				var m = JSON.parse(evt.data)
 				typeFunc = {
 					
-					}
 				}
 				if (typeFunc[m.type] != undefined) {
 					typeFunc[m.type](m.data)
 				}
 			}
 			c.sock.onopen = function() {
-				c.send("init", c.name)
+				c.send("connect", c.name)
 			}
 			c.sock.onclose = function() { 
 				alert("Disconnected from Server")
@@ -27,6 +27,16 @@ var Client = {
 		c.sock.send(JSON.stringify({type : t, data : m}))
 	}
 }
-var c = Client
 
-get = function(id) {return document.getElementById(id)}
+var Render = {
+	cvs: document.getElementById("canvas"),
+	ctx: document.getElementById("canvas").getContext("2d")
+	tick: function() {
+		r.context.clearRect(0, 0, r.canvas.width, r.canvas.height)
+	}
+}
+
+var c = Client
+var r = Render
+
+var get = function(id) {return document.getElementById(id)}
