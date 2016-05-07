ac76adc76a5cfc68e721db2f93032b2bfeacaeb3
Chaotic-Potato <andy825825@gmail.com>
2016-05-06 20:16:58 -0500

    Fix Conflict I Hope

--- Client/script.js
+++ Client/script.js
@@ -31,7 +31,6 @@ var Client = {
 	tick: function() {
 		r.tick()
 	}
-<<<<<<< HEAD
 }
 
 var Render = {
@@ -44,20 +43,6 @@ var Render = {
 	}
 }
 
-=======
-}
-
-var Render = {
-	cvs: document.getElementById("canvas"),
-	ctx: document.getElementById("canvas").getContext("2d"),
-	tick: function() {
-		r.ctx.clearRect(0, 0, 1280, 720)
-		r.ctx.fillStyle = "white"
-		r.ctx.fillRect(100, c.y, 50, 200)
-	}
-}
-
->>>>>>> master
 var c = Client
 var r = Render
 
