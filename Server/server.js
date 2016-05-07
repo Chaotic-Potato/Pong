f2f6dbf4988f2a8c6c0e9866a0e509ab73098181
Sam Quinn <lazersmoke@gmail.com>
2016-05-06 21:54:59 -0500

    Fixed sending too much client data on lobby update

--- Server/server.js
+++ Server/server.js
@@ -61,7 +61,7 @@ var Server = {
   	c.sendUTF(JSON.stringify({type : t, data : m}))
 	},
   updateLobby: function(){
-    s.sendall("lobby", s.clients)
+    s.sendall("lobby", s.clients.map(function(a){return a.name}))
   },
 	tick: function() {}
 }
